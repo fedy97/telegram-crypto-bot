20367faed2268305298ac991b3b2c12680700ac6
fedy97 <frc.morreale@gmail.com>
2023-08-11 00:21:25 +0200

    fixed prices

--- src/main/java/org/bot/models/factory/CoinFactory.java
+++ src/main/java/org/bot/models/factory/CoinFactory.java
@@ -14,7 +14,7 @@ public class CoinFactory {
             coin.setCoinName(raw.split(" ")[0]);
             coin.setTicker(raw.split("\\(")[1].split("\\)")[0]);
             coin.setLink("https://www.coingecko.com" + raw.split("\"width: 115px;\" href=\"")[1].split("\"")[0]);
-            coin.setPrice(Double.parseDouble(raw.split("<td data-sort='")[1].split("'")[0]));
+            coin.setPrice(Double.parseDouble(raw.split("<td data-sort=\"")[1].split("\"")[0]));
             coin.setChange24(raw.split("data-show-solid-arrow=\"false\" data-formatted=\"false\">")[2].split("<")[0]);
         } catch (Exception e) {
             // continue
